d2ef2ec80df64d1797c985a5e8032a0fb2012c9a
Mahmudulhasan162 <mahmudulhasanshafi2345@gmail.com>
2024-02-17 14:37:02 +0600

    New collection of data has been added as orderCollection where every data of order will be stored and user based data can also be found because I have added find some method in this part

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app= express();
 const port = process.env.PORT || 5000;
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 //middleware
 app.use(cors());
@@ -28,11 +28,21 @@ async function run() {
     await client.connect();
     const serviceCollection = client.db('carDoctor').collection('services');
     const productCollection = client.db('carDoctor').collection('products');
+    const orderCollection = client.db('carDoctor').collection('orders');
 
     app.get('/services', async(req, res)=>{
         const cursor = serviceCollection.find();
         const result = await cursor.toArray();
         res.send(result);
+    })
+    app.get('/services/:id', async(req, res)=>{
+      const id= req.params.id
+      const query= {_id: new ObjectId(id)}
+      const option = {
+        projection: {title: 1, price: 1, service_id:1, img: 1 },
+      };
+      const result = await serviceCollection.findOne(query, option);
+      res.send(result)
     })
     app.get('/products', async(req, res)=>{
         const cursor = productCollection.find();
@@ -40,6 +50,27 @@ async function run() {
         res.send(result);
     })
 
+    //Orders
+    app.post('/orders', async(req, res)=>{
+      const order= req.body;
+      console.log(order);
+      const result = await orderCollection.insertOne(order);
+      res.send(result)
+
+    })
+    app.get('/orders', async(req, res)=> {
+      let query= {}
+      console.log(req.query);
+      if(req.query?.email){
+        query= {email: req.query.email}
+        console.log(query);
+      }
+      const cursor = orderCollection.find(query);
+      //console.log(result);
+      const result = await cursor.toArray();
+      res.send(result)
+
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
